a7200a4549fcb1a77d8dad9c2edc64b336adb9cf
ATHT138 <anhthtservice@gmail.com>
2024-10-14 13:40:47 +0700

    feat: filter fix

--- src/views/productpage/product-list/filter.tsx
+++ src/views/productpage/product-list/filter.tsx
@@ -38,7 +38,11 @@ const Filter = () => {
     };
   }, [pageSizeTypes]);
 
-  const { data: software, error: softwareError } = useSoftware({
+  const {
+    data: software,
+    error: softwareError,
+    fetchNextPage,
+  } = useSoftware({
     options: optionSoftware,
   });
 
@@ -130,32 +134,34 @@ const Filter = () => {
           </Button>
         </div>
         <div className="flex flex-wrap gap-2">
-          {software?.data.map((item) => (
-            <Badge
-              key={item.id}
-              variant={
-                filter.softwareIds?.find((sw) => item.id === sw)
-                  ? "default"
-                  : "outline"
-              }
-              className="hover:cursor-pointer hover:bg-primary hover:text-primary-foreground"
-              onClick={() => {
-                setFilter({
-                  ...filter,
-                  softwareIds: filter.softwareIds?.includes(item.id)
-                    ? filter.softwareIds?.filter((sw) => sw !== item.id)
-                    : [...(filter.softwareIds || []), item.id],
-                });
-              }}
-            >
-              {item.name}
-            </Badge>
-          ))}
+          {software?.pages.map((data) =>
+            data.data.map((item) => (
+              <Badge
+                key={item.id}
+                variant={
+                  filter.softwareIds?.find((sw) => item.id === sw)
+                    ? "default"
+                    : "outline"
+                }
+                className="hover:cursor-pointer hover:bg-primary hover:text-primary-foreground"
+                onClick={() => {
+                  setFilter({
+                    ...filter,
+                    softwareIds: filter.softwareIds?.includes(item.id)
+                      ? filter.softwareIds?.filter((sw) => sw !== item.id)
+                      : [...(filter.softwareIds || []), item.id],
+                  });
+                }}
+              >
+                {item.name}
+              </Badge>
+            ))
+          )}
         </div>
         <Button
           className="w-full space-x-2"
           variant="outline"
-          onClick={() => setPageSizeSoftware(100)}
+          onClick={() => fetchNextPage()}
         >
           <ChevronDown size={24} />
           <span>See more</span>
@@ -205,7 +211,7 @@ const Filter = () => {
         <Button
           className="w-full space-x-2"
           variant="outline"
-          onClick={() => setPageSizeTypes(100)}
+          onClick={() => fetchNextPage}
         >
           <ChevronDown size={24} />
           <span>See more</span>
